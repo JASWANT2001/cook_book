f3f573e1bd774024ae8d99b0be4169a2052a58b3
JASWANT2001 <k.jaswant2305@gmail.com>
2024-07-28 17:27:29 +0530

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Create from "./Create";
+import Menu from "./Menu";
+import Home from "./Home";
+import "./App.css";
+import FileUploadForm from "./FileUploadForm";
+
 
 function App() {
   return (
     <>
-    
+      <div className="bl">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/create" element={<Create />} /> */}
+            <Route path="/create" element={<FileUploadForm/>} />
+            <Route path="/menu" element={<Menu />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
